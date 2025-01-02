598a79bbdf50db08329fcf9515bf19eda8e6b1d9
Your Nameyoungwoongko <youngwoong.ko@galaglobal.com>
2025-01-02 10:27:26 +0900

    수정 중

--- src/components/ui/popup/UIPopPurchasePoint.tsx
+++ src/components/ui/popup/UIPopPurchasePoint.tsx
@@ -1,6 +1,5 @@
 import { useDispatch } from "react-redux";
 import * as globalSlice from "../../../redux/globalSlice";
-import { useEffect } from "react";
 import { displayPopType } from "common/define";
 
 const UIPopPurchasePoint = () => {
