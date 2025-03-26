2e3cdac7f36e898a53d3c6bf6db50ac03519b698
zaeyonn <147461956+zaeyonn@users.noreply.github.com>
2025-03-26 19:46:21 +0900

    feat: video url 수정

--- src/pages/SeriesPlayerPage.tsx
+++ src/pages/SeriesPlayerPage.tsx
@@ -753,7 +753,7 @@ const SeriesPlayerPage = ({}) => {
         setVideoLoading(true);
 
         const blobUrl: any = await convertToBlobURL(
-          `${import.meta.env.VITE_SERVER_URL}/resources/videos/${currentEp?.series_id}/${
+          `${import.meta.env.VITE_SERVER_URL}/videos/${currentEp?.series_id}/${
             currentEp?.video
           }`
         );
